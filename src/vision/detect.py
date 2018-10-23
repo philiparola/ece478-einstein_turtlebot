94f3b92a4856d736cc167fa3ec100b51b2a2dee7
Philip Arola <philiparola@hotmail.com>
2018-10-23 16:38:20 -0700

    Webcam enabled, loops w/out display

--- src/vision/detect.py
+++ src/vision/detect.py
@@ -2,30 +2,35 @@ import cv2
 import sys
 
 # Get user supplied values
-imagePath = sys.argv[1]
 cascPath = "haarcascade_frontalface_default.xml"
+webcam = cv2.VideoCapture(0)
 
 # Create the haar cascade
 faceCascade = cv2.CascadeClassifier(cascPath)
 
-# Read the image
-image = cv2.imread(imagePath)
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+while webcam.isOpened(): # try to get the first frame
+    rval, frame = webcam.read()
+#else:
+#    rval = False
 
-# Detect faces in the image
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30, 30)
-    #flags = cv2.CV_HAAR_SCALE_IMAGE
-)
 
-print("Found {0} faces!".format(len(faces)))
+    # Read the image
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-# Draw a rectangle around the faces
-for (x, y, w, h) in faces:
-    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30, 30)
+        #flags = cv2.CV_HAAR_SCALE_IMAGE
+    )
 
-cv2.imshow("Faces found", image)
-cv2.waitKey(0)
+    print("Found {0} faces!".format(len(faces)))
+
+    # Draw a rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    #cv2.imshow("Faces found", frame)
+    #cv2.waitKey(0)
