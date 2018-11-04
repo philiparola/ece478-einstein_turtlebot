2e15742caa59bca96ceb92b31c4d9fbbdbb09a50
Philip Arola <parola@pdx.edu>
2018-11-04 13:25:57 -0800

    Moved to catkin workspace
    So I can actually run things

---
+++ src/catkin_ws/build/gesture_controls/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/parola/ece478-turtlebot/src/catkin_ws/src/gesture_controls
+# Build directory: /home/parola/ece478-turtlebot/src/catkin_ws/build/gesture_controls
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
