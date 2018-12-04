96bde54d92605588483f31865f7ca0fa59089dc3
Philip Arola <parola@pdx.edu>
2018-12-04 01:12:13 -0800

    Google talks back!
    Google DialogFlow now speaks to you.  This is rudimentary, issuing bash
    commands via Python.  Need to figure out native Python ROS services.

--- catkin_ws/src/voice_recognition/src/recognition.py
+++ catkin_ws/src/voice_recognition/src/recognition.py
@@ -2,16 +2,26 @@
 
 import os, sys
 #import record
+import rospy
 import dialogflow
+#from speech_synthesis.srv import *
 
 def recognition():
     #record.record()
     os.system("./record.py")
     response = dialogflow.dialogflow_main()
-    print("test" + response)
+    #synthesize_voice = rospy.ServiceProxy('speech_synthesis', synthesis_service)
+    #return speech_synthesis()
+    if len(response) is 0:
+        response = "a"
+    command = "rosservice call \/speech_synthesis \'" + response + "\'"
+    print(command)  # Somehow fixes a type error?
+    os.system(str(command))
+
 
 if __name__ == '__main__':
+    rospy.wait_for_service('speech_synthesis')
     while(True):
         recognition()
-        input("Press Enter to continue...")
+        raw_input("Press Enter to continue...")
 
